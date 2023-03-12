4630868d558767b9d8b00ab9f9dc6e4f467f28b1
Maksim Kornakov <makskornakov@ya.ru>
2023-03-12 01:52:09 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FooterContainer } from './Footer.styled';
 export default function FooterWrap() {
   return (
     <FooterContainer>
-      <p>© 2021 Data analyzer</p>
+      <p>© 2023 Data analyzer</p>
     </FooterContainer>
   );
 }
