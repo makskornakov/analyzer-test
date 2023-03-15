ee8a7278fd8b91907a489172414324c6c458569c
Maksim Kornakov <makskornakov@ya.ru>
2023-03-15 19:23:31 +0100

    hmmm

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -9,6 +9,7 @@ export interface BoardItemProps {
   id: string;
   content: string;
 }
+
 export interface BoardData {
   itemArrays: BoardItemProps[][]; // array of arrays of items
 }
@@ -32,68 +33,52 @@ const onStopFunction: DraggableEventHandler = (e, data) => {
   console.log('onStop', e, data);
 };
 
-function BoardItem({ id, content }: BoardItemProps) {
-  const itemRef = useRef<HTMLDivElement>(null);
+function BoardItem({
+  id,
+  content,
+  dragFunction,
+}: BoardItemProps & {
+  dragFunction: (e: MouseEvent | TouchEvent, id: string) => void;
+}) {
+  const [controlPosition, setControlPosition] = useState<ControlPosition>({
+    x: 0,
+    y: 0,
+  });
 
   const onDragFunction: DraggableEventHandler = (e, data) => {
-    // mouse coordinates
-    const mouseCoordinates = e as MouseEvent;
-    const boardContainer = document.getElementById('board-container');
-    if (!boardContainer) return;
-    const boardContainerRect = boardContainer.getBoundingClientRect();
-    const mouseCords = {
-      x: mouseCoordinates.clientX - boardContainerRect.x,
-      y: mouseCoordinates.clientY - boardContainerRect.y,
+    const thisId = data.node.id;
+    dragFunction(e as MouseEvent, thisId);
+
+    const cords = {
+      x: data.x,
+      y: data.y,
     };
-
-    console.log(mouseCords);
-
-    // setControlPosition(coordinates);
+    setControlPosition(cords);
   };
-
   return (
     <Draggable
       defaultPosition={{ x: 0, y: 0 }}
-      // position={}
+      position={controlPosition}
       onStart={onStartFunction}
       onDrag={onDragFunction}
       onStop={onStopFunction}
     >
-      <Item id={id} ref={itemRef}>
+      <Item id={id}>
         <span>{content}</span>
       </Item>
     </Draggable>
   );
 }
 
-function BoardList({ items }: { items: BoardItemProps[] }) {
-  // const [itemsState, setItemsState] = useState<
-  //   { id: string; position: DOMRect }[]
-  // >([]);
-
-  // useEffect(() => {
-  //   const boardContainer = document.getElementById('board-container');
-  //   if (!boardContainer) return;
-  //   const boardContainerRect = boardContainer.getBoundingClientRect();
-  //   const itemsState = items.map((item) => {
-  //     const itemElement = document.getElementById(item.id);
-  //     if (!itemElement) return;
-  //     const itemRect = itemElement.getBoundingClientRect();
-  //     const itemPosition = {
-  //       x: itemRect?.x - boardContainerRect?.x,
-  //       y: itemRect?.y - boardContainerRect?.y,
-  //     };
-  //     return {
-  //       id: item.id,
-  //       position: itemPosition,
-  //     };
-  //   });
-  //   setItemsState(itemsState);
-  // }, [items]);
-
+function BoardList({
+  items,
+  dragFunction,
+}: {
+  items: BoardItemProps[];
+  dragFunction: (e: MouseEvent | TouchEvent, id: string) => void;
+}) {
   return (
     <div
-      // ref={containerRef}
       style={{
         display: 'flex',
         flexDirection: 'column',
@@ -103,7 +88,12 @@ function BoardList({ items }: { items: BoardItemProps[] }) {
       }}
     >
       {items.map((item) => (
-        <BoardItem key={item.id} id={item.id} content={item.content} />
+        <BoardItem
+          key={item.id}
+          id={item.id}
+          content={item.content}
+          dragFunction={dragFunction}
+        />
       ))}
     </div>
   );
@@ -114,7 +104,7 @@ export default function Board({
 }: {
   boardData: BoardData;
 }): JSX.Element {
-  const [ItemsPosition, setItemsPosition] = useState<
+  const [ItemsPositions, setItemsPositions] = useState<
     {
       id: string;
       position: {
@@ -156,12 +146,47 @@ export default function Board({
         });
       });
     });
-    setItemsPosition(returnArray);
+    setItemsPositions(returnArray);
   }, [boardData]);
 
   useEffect(() => {
-    console.log(ItemsPosition);
-  }, [ItemsPosition]);
+    console.log(ItemsPositions);
+  }, [ItemsPositions]);
+  const checkIntersection = (e: MouseEvent | TouchEvent, id: string) => {
+    // mouse coordinates
+    const mouseCoordinates = e as MouseEvent;
+    const boardContainer = document.getElementById('board-container');
+    if (!boardContainer) return;
+    const boardContainerRect = boardContainer.getBoundingClientRect();
+    const mouseCords = {
+      x: mouseCoordinates.clientX - boardContainerRect.x,
+      y: mouseCoordinates.clientY - boardContainerRect.y,
+    };
+
+    // go through all items and check if mouse is inside
+    ItemsPositions.forEach((item) => {
+      if (
+        mouseCords.x > item.position.x1 &&
+        mouseCords.x < item.position.x2 &&
+        mouseCords.y > item.position.y1 &&
+        mouseCords.y < item.position.y2
+      ) {
+        // set the background color of the item to red
+        const itemElement = document.getElementById(item.id);
+        if (!itemElement || item.id === id) return;
+        itemElement.style.backgroundColor = 'red';
+      } else {
+        // set the background color of the item to white
+        const itemElement = document.getElementById(item.id);
+        if (!itemElement) return;
+        itemElement.style.backgroundColor = 'white';
+      }
+    });
+
+    console.log(mouseCords);
+
+    // setControlPosition(coordinates);
+  };
 
   return (
     <>
@@ -169,7 +194,7 @@ export default function Board({
       <BoardContainer id="board-container">
         {boardData.itemArrays.map((itemArray, index) => (
           <div key={index}>
-            <BoardList items={itemArray} />
+            <BoardList items={itemArray} dragFunction={checkIntersection} />
           </div>
         ))}
       </BoardContainer>
