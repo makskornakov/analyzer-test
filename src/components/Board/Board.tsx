bf67c882f10a674b1039802be6bbbfb4a2324260
Maksim Kornakov <makskornakov@ya.ru>
2023-03-19 16:43:03 +0100

    ьщкуц

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react';
+import { DraggableEvent, DraggableData } from 'react-draggable';
 import BoardList from './BoardList';
 import { exampleBoardContent } from './example';
 
@@ -8,13 +10,114 @@ export interface BoardItem {
 }
 export type BoardListContent = BoardItem[];
 export type BoardContent = Map<string, BoardListContent>;
+export interface Position {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+function getRect(element: HTMLElement, container: HTMLElement): Position {
+  const rect = element.getBoundingClientRect();
+  const containerRect = container.getBoundingClientRect();
+  return {
+    x1: rect.x - containerRect.x,
+    y1: rect.y - containerRect.y,
+    x2: rect.x + rect.width - containerRect.x,
+    y2: rect.y + rect.height - containerRect.y,
+  };
+}
+
+function getIntersectionPercentage(rect: Position, checkableRect: Position) {
+  // return the percentage of rect that is inside checkableRect
+  const x1 = Math.max(rect.x1, checkableRect.x1);
+  const y1 = Math.max(rect.y1, checkableRect.y1);
+  const x2 = Math.min(rect.x2, checkableRect.x2);
+  const y2 = Math.min(rect.y2, checkableRect.y2);
+  const intersectionArea = Math.max(0, x2 - x1) * Math.max(0, y2 - y1);
+  const rectArea = (rect.x2 - rect.x1) * (rect.y2 - rect.y1);
+  return intersectionArea / rectArea;
+}
 
 export default function Board() {
+  const container = useRef<HTMLDivElement>(null);
+  const [boardContent, setBoardContent] =
+    useState<BoardContent>(exampleBoardContent);
+
+  const [boardPositions, setBoardPositions] = useState<Map<string, Position>>(
+    new Map()
+  );
+
+  const [itemPositions, setItemPositions] = useState<Map<string, Position>>(
+    new Map()
+  );
+
+  useEffect(() => {
+    const boardPositions = new Map();
+    const itemPositions = new Map();
+    Array.from(boardContent.keys()).forEach((key) => {
+      const boardList = document.getElementById(key);
+      if (boardList) {
+        const rect = getRect(boardList, container.current as HTMLDivElement);
+        boardPositions.set(key, rect);
+      }
+      // set item positions
+      const boardListContent = boardContent.get(key) as BoardListContent;
+      boardListContent.forEach((item) => {
+        const itemElement = document.getElementById(item.id.toString());
+        if (itemElement) {
+          const rect = getRect(
+            itemElement,
+            container.current as HTMLDivElement
+          );
+          itemPositions.set(item.id.toString(), rect);
+        }
+      });
+    });
+    setBoardPositions(boardPositions);
+    setItemPositions(itemPositions);
+  }, [boardContent]);
+
+  function handleDrag(e: DraggableEvent, data: DraggableData) {
+    const { x, y } = data;
+    const id = data.node.id;
+    const initialPosition = itemPositions.get(id);
+    if (!initialPosition) return;
+    const currentPosition = {
+      x1: initialPosition.x1 + x,
+      y1: initialPosition.y1 + y,
+      x2: initialPosition.x2 + x,
+      y2: initialPosition.y2 + y,
+    };
+    // check intersection with all board lists and if there is an intersection set background color of the board list to rgba(255, 0, 0, 0.5)
+    boardPositions.forEach((rect, key) => {
+      const intersectionPercentage = getIntersectionPercentage(
+        currentPosition,
+        rect
+      );
+      const boardList = document.getElementById(key);
+      if (boardList) {
+        boardList.style.backgroundColor =
+          intersectionPercentage > 0.1
+            ? 'rgba(255, 0, 255, 0.2)'
+            : 'rgba(0, 0, 0, 0)';
+        boardList.style.outlineWidth =
+          intersectionPercentage > 0.1 ? '4px' : '1px';
+      }
+      // if (intersectionPercentage !== 0) console.log(intersectionPercentage);
+    });
+  }
+  function onDragStart(e: DraggableEvent, data: DraggableData) {}
+
+  function onDragStop(e: DraggableEvent, data: DraggableData) {}
+
   return (
     <>
       <h2>Board</h2>
       <div
+        ref={container}
         style={{
+          position: 'relative',
           display: 'flex',
           flexDirection: 'row',
           justifyContent: 'space-evenly',
@@ -23,13 +126,18 @@ export default function Board() {
           outline: '1px solid #f55500',
         }}
       >
-        {Array.from(exampleBoardContent.keys()).map((key) => (
+        {Array.from(boardContent.keys()).map((key) => (
           <BoardList
             key={key}
-            boardList={exampleBoardContent.get(key) as BoardListContent}
+            id={key}
+            boardList={boardContent.get(key) as BoardListContent}
+            itemPositions={itemPositions}
             width="20em"
             itemHeight="3em"
             gap="1em"
+            handleDrag={handleDrag}
+            onDragStart={onDragStart}
+            onDragStop={onDragStop}
           />
         ))}
       </div>
--- src/components/Board/BoardList.tsx
+++ src/components/Board/BoardList.tsx
@@ -1,24 +1,37 @@
-import { BoardListContent } from './Board';
+import { BoardListContent, Position } from './Board';
 import Draggable from 'react-draggable';
 
 export default function BoardList({
+  id,
   boardList,
+  itemPositions,
   width,
   itemHeight,
   gap,
+  handleDrag,
+  onDragStart,
+  onDragStop,
 }: {
+  id: string;
   boardList: BoardListContent;
+  itemPositions: Map<string, Position>;
   width: string;
   itemHeight: string;
   gap: string;
+  handleDrag: (e: any, data: any) => void;
+  onDragStart: (e: any, data: any) => void;
+  onDragStop: (e: any, data: any) => void;
 }) {
   return (
     <div>
       <h3>Board List</h3>
       <div
+        id={id}
         style={{
           width: width,
-          outline: '1px solid red',
+          outlineWidth: '1px',
+          outlineStyle: 'solid',
+          outlineColor: 'white',
         }}
       >
         {boardList.map((item) => (
@@ -29,19 +42,50 @@ export default function BoardList({
             nodeRef={undefined}
             // grid={[25, 25]}
             // scale={1}
-            // onStart={this.handleStart}
-            // onDrag={this.handleDrag}
-            // onStop={this.handleStop}
+            onStart={(e, data) => {
+              const id = data.node.id;
+              const initialPosition = itemPositions.get(id);
+              if (!initialPosition) return;
+              const dragElement = document.getElementById(id);
+              if (dragElement) {
+                dragElement.style.position = 'absolute';
+                dragElement.style.top = initialPosition.y1 + 'px';
+                dragElement.style.left = initialPosition.x1 + 'px';
+                dragElement.style.zIndex = '100';
+                dragElement.style.marginTop = '0';
+              }
+              onDragStart(e, data);
+            }}
+            onDrag={(e, data) => {
+              handleDrag(e, data);
+            }}
+            onStop={(e, data) => {
+              const { x, y } = data;
+              const id = data.node.id;
+              const initialPosition = itemPositions.get(id);
+              if (!initialPosition) return;
+              const dragElement = document.getElementById(id);
+              if (dragElement) {
+                dragElement.style.position = 'initial';
+                dragElement.style.top = 'initial';
+                dragElement.style.left = 'initial';
+                dragElement.style.zIndex = 'initial';
+                dragElement.style.marginTop = gap;
+              }
+
+              onDragStop(e, data);
+            }}
           >
             <div
               id={item.id.toString()}
               style={{
-                width: '100%',
+                width: width,
                 height: itemHeight,
                 outline: '1px solid blue',
                 marginTop: gap,
 
                 display: 'flex',
+                background: 'darkgray',
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
